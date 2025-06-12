1ce9b8f392e245cec4ebdb0b9c2f665f31abbae7
Logan <pix.59700@gmail.com>
2025-06-12 02:16:00 +0200

    Add client communication

---
+++ REST-API-Unity/Assets/_Core/Scripts/ApiTest/Client.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace logandlp.apitest
+{
+    using restapi;
+    
+    public class Client : MonoBehaviour
+    {
+        [SerializeField] private string _message;
+
+        [ContextMenu("Send Message")]
+        public void Send()
+        {
+            Data data = new()
+            {
+                Message = _message
+            };
+
+            StartCoroutine(RestAPIHandler<Data>.Post(data));
+        }
+    }
+}
